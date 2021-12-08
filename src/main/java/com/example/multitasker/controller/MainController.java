20f6d0368faae508e6cb7934bb78ef84a69be8ea
Laurier Rose <laurence.bellebouche@edu.esiee-it.fr>
2021-12-08 09:41:34 +0100

    First commit

---
+++ src/main/java/com/example/multitasker/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.example.multitasker.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private MenuBar mbmenuBar;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
+
+
