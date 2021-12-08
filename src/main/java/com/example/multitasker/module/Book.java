3b36dd3129ff7a382e993a4c7e5aa85b204c174f
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2021-12-08 12:22:16 +0100

    Creation of Library class, controller and application

--- src/main/java/com/example/multitasker/module/Book.java
+++ src/main/java/com/example/multitasker/module/Book.java
@@ -1,4 +1,21 @@
 package com.example.multitasker.module;
 
 public class Book {
+
+    private String title;
+    private String author;
+    private int released;
+    private int[] location;
+    private String summary;
+    private String cover;
+
+    public Book(String title, String author, int released, int column, int row, String summary) {
+        this.title = title;
+        this.author = author;
+        this.released = released;
+        this.location[0] = column;
+        this.location[1] = row;
+        this.summary = summary;
+        this.cover = null;
+    }
 }
