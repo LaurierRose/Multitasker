1e9e260979fced66450ef7222788ff200eb6961e
Laurier Rose <laurence.bellebouche@edu.esiee-it.fr>
2021-12-08 12:28:32 +0100

    Army structure

---
+++ src/main/java/com/example/multitasker/controller/ArmyController.java
@@ -0,0 +1,33 @@
+package com.example.multitasker.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ArmyController implements Initializable {
+
+    @FXML
+    private Button btncreate;
+
+    @FXML
+    private TreeView<?> tvarmy;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btncreate.setOnMouseClicked(action->{
+            //Create root item
+            String armyName =
+            TreeItem rootItem = new TreeItem<>(armyName);
+            tvarmy.setRoot(rootItem);
+        });
+
+
+    }
+
+
+}
