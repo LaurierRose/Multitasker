eda3ac8d3c48a0cda325d5d8a0e8e0ca3c6e198a
Laurier Rose <laurence.bellebouche@edu.esiee-it.fr>
2021-12-08 10:56:27 +0100

    MainMenu app xml structure done

--- src/main/java/com/example/multitasker/MainApplication.java
+++ src/main/java/com/example/multitasker/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Welcome to multitasker!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/multitasker/controller/MainController.java
+++ src/main/java/com/example/multitasker/controller/MainController.java
@@ -2,7 +2,10 @@ package com.example.multitasker.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,9 +13,25 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
 
     @FXML
+    private Button btnIMC;
+
+    @FXML
+    private Button btnarmy;
+
+    @FXML
+    private Button btnconverter;
+
+    @FXML
+    private Button btnlibrary;
+
+    @FXML
+    private AnchorPane mainWindow;
+
+    @FXML
     private MenuBar mbmenuBar;
 
-
+    @FXML
+    private MenuItem miquit;
 
 
     @Override
